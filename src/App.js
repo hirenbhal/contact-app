39e675a1c38bccb1e4c229a14765630fb9d3c6ff
hirenbhal <iamhiren5@gmail.com>
2022-03-26 16:11:33 +0530

    frontend setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { List,Avatar,Typography } from "antd";
+import { useState, useEffect } from "react";
 
-function App() {
+const { Title } = Typography;
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const dummy = [
+    {
+      title: "Title 1",
+    },
+    {
+      title: "Title 2",
+    },
+    {
+      title: "Title 3",
+    },
+    {
+      title: "Title 4",
+    },
+    {
+      title: "Title 5",
+    },
+    {
+      title: "Title 6",
+    },
+  ];
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api?results=10")
+      .then((response) => response.json())
+      .then((actualData) => {
+        setData(actualData.results);
+        console.log(data);
+
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title className="title">Contact App</Title>
+      <List
+      className="List"
+       itemLayout="horizontal"
+        dataSource={data}
+        renderItem={(item) => <List.Item>
+          <List.Item.Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title={<a href="https://ant.design">{item.name.title} {item.name.first} {item.name.last} </a>}
+          description={item.email}
+        />
+        </List.Item>}
+      />
     </div>
   );
-}
+};
 
 export default App;
